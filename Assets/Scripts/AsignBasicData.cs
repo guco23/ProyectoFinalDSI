7f33d6d1f85ed4a66d82c110bc3feae27fa7d8ae
Diego Montenegro <dmonoficial22@gmail.com>
2024-05-07 18:28:31 +0200

    cosillas. esta roto. tengo que mirar como funciona enum.

--- Assets/Scripts/AsignBasicData.cs
+++ Assets/Scripts/AsignBasicData.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -8,15 +6,40 @@ public class AsignBasicData : MonoBehaviour
     UIDocument doc;
     VisualElement root;
     VisualElement topBar;
+    
+    //La pantalla principal mostrada ahora mismo
+    MainScreen currentMainScreen;
+    VisualElement[] mainScreenBarElement;
+
+    enum MainScreen
+    {
+        STAT = 0,
+        INV = 1,
+        DATA = 2,
+        MAP = 3,
+        RADIO = 4
+    }
 
     void Start()
     {
+        mainScreenBarElement = new VisualElement[5];
         doc = GetComponent<UIDocument>();
+        currentMainScreen = MainScreen.STAT;
     }
 
     private void OnEnable()
     {
         VisualElement root = GetComponent<UIDocument>().rootVisualElement;
         topBar = root.Q("topBar");
+
+        SetUpBasicElems();
+    }
+
+    //Crea la barra superior y otros elementos basicos que estarán en pantalla
+    void SetUpBasicElems()
+    {
+        //Establece los MainScreenElements
+        //No entiendo los enum lol
+        mainScreenBarElement[] = topBar.Q("q");
     }
 }
